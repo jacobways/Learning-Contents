41ca99a1b940537cf291a485b48e1ae0631cbd49
jacobways <jacobkjy0501@gmail.com>
2022-05-10 14:54:10 +0900

    Nextjs의 Layout

--- NomadCoders/next-js/pages/_app.js
+++ NomadCoders/next-js/pages/_app.js
@@ -1,15 +1,12 @@
 import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 
 export default function App({Component, pageProps}) {
   return (
     <div>
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   );
 }
