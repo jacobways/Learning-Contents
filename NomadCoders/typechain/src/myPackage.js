0e5334e8ec48f23e138d1d7422e31090b4dfc153
jacobways <jacobkjy0501@gmail.com>
2022-07-30 23:31:11 +0900

    JSDoc 사용

--- NomadCoders/typechain/src/myPackage.js
+++ NomadCoders/typechain/src/myPackage.js
@@ -1,7 +1,20 @@
+/**
+ * 
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean} 
+ */
+
 export function init(config) {
   return true;
 }
 
+/**
+ * 
+ * @param {number} code 
+ * @returns 
+ */
 export function exit(code) {
   return code + 1;
 }
